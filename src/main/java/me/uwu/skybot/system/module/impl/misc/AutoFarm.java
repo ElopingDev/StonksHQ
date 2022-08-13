c5a4ceaadcfcda75c4ad243234b81ed28489b77c
UwU <mmimi0593@gmail.com>
2022-08-14 00:20:40 +0200

    I commited (murder)

--- src/main/java/me/uwu/skybot/system/module/impl/misc/AutoFarm.java
+++ src/main/java/me/uwu/skybot/system/module/impl/misc/AutoFarm.java
@@ -4,50 +4,177 @@ import fr.shyrogan.post.receiver.annotation.Subscribe;
 import me.uwu.skybot.SkyBot;
 import me.uwu.skybot.event.impl.EventRender;
 import me.uwu.skybot.event.impl.EventUpdate;
+import me.uwu.skybot.utils.Timer;
 import me.xtrm.skybot.system.module.Category;
 import me.xtrm.skybot.system.module.Module;
-import me.xtrm.skybot.utils.RenderUtils;
-import net.minecraft.block.state.BlockState;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldRenderer;
+import me.xtrm.skybot.utils.Renderer;
+import net.minecraft.block.Block;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
-import org.lwjgl.opengl.GL11;
+import net.minecraft.util.MathHelper;
 
-@Module.Metadata(
-        id = "auto-farm",
-        category = Category.MISCELLANEOUS
-)
+import java.awt.*;
+
+@Module.Metadata(id = "auto-farm", category = Category.MISCELLANEOUS)
 public class AutoFarm extends Module {
+    private final Timer timer = new Timer();
+    private int dodo = 0;
+    private boolean goRight = true;
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        goRight = true;
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        mc.gameSettings.keyBindForward.pressed = false;
+        mc.gameSettings.keyBindBack.pressed = false;
+        mc.gameSettings.keyBindLeft.pressed = false;
+        mc.gameSettings.keyBindRight.pressed = false;
+        mc.gameSettings.keyBindAttack.pressed = false;
+    }
+
     @Subscribe
     public void onUpdate(EventUpdate.Pre eventUpdate) {
-        EnumFacing facing = EnumFacing.values()[SkyBot.INSTANCE.getConfig().enumFacingOrd + 2];
+        mc.gameSettings.keyBindAttack.pressed = true;
 
-        IBlockState rightBlock = mc.theWorld.getBlockState(mc.thePlayer.getPosition().add(facing.getDirectionVec().getZ() + 0.5f, 0, facing.getDirectionVec().getX() + 0.5f));
-        IBlockState bottomBlock = mc.theWorld.getBlockState(mc.thePlayer.getPosition().add(0, -0.8f, 0)); // Nyaa~
+        EnumFacing facing = EnumFacing.values()[SkyBot.INSTANCE.getConfig().enumFacingOrd + 2];
+        BlockPos[] pos = getPositions();
+
+        BlockPos bottomPos = new BlockPos(
+                mc.thePlayer.posX,
+                mc.thePlayer.posY - 0.5f,
+                mc.thePlayer.posZ
+        );
+
+        Block bottomBlock = mc.theWorld.getBlockState(bottomPos).getBlock();
+
+        System.out.println(bottomBlock.getLocalizedName() + "cc");
+
+        if (dodo > 0) {
+            dodo--;
+            //mc.gameSettings.keyBindRight.pressed = false;
+            //mc.gameSettings.keyBindLeft.pressed = false;
+        } else {
+            if (bottomBlock.getUnlocalizedName().contains("Portal")) {
+                mc.gameSettings.keyBindRight.pressed = false;
+                mc.gameSettings.keyBindLeft.pressed = false;
+                mc.gameSettings.keyBindJump.pressed = true;
+            } else {
+                mc.gameSettings.keyBindJump.pressed = false;
+                if (goRight) {
+                    if (mc.thePlayer.isCollidedHorizontally && !mc.theWorld.isAirBlock(pos[1])) {
+                        goRight = !goRight;
+                        dodo = 20;
+                    }
+                } else {
+                    if (mc.thePlayer.isCollidedHorizontally && !mc.theWorld.isAirBlock(pos[0])) {
+                        goRight = !goRight;
+                        dodo = 20;
+                    }
+                }
+            }
+        }
+
+        if (dodo <= 0) {
+            if (goRight) {
+                mc.gameSettings.keyBindRight.pressed = true;
+                mc.gameSettings.keyBindLeft.pressed = false;
+            } else {
+                mc.gameSettings.keyBindRight.pressed = false;
+                mc.gameSettings.keyBindLeft.pressed = true;
+            }
+        }
+
+        mc.gameSettings.keyBindForward.pressed = true;
 
         mc.thePlayer.rotationYaw = facing.getHorizontalIndex() * 90;
         mc.thePlayer.rotationPitch = -3.1f;
     }
 
     @Subscribe
+    public void onRender2D(EventRender.R2D eventRender) {
+        int i = 0;
+        fr.drawStringWithShadow("AutoFarm", 2, 2 + (i++ * fr.FONT_HEIGHT), -1);
+        BlockPos pos = new BlockPos(
+                mc.thePlayer.posX,
+                mc.thePlayer.posY,
+                mc.thePlayer.posZ
+        );
+        fr.drawStringWithShadow("ppos   => " + pos, 2, 2 + (i++ * fr.FONT_HEIGHT), -1);
+        BlockPos floor = new BlockPos(
+                MathHelper.floor_double(pos.getX()),
+                MathHelper.floor_double(pos.getY()),
+                MathHelper.floor_double(pos.getZ())
+        );
+        fr.drawStringWithShadow("floord => " + floor, 2, 2 + (i++ * fr.FONT_HEIGHT), -1);
+        fr.drawStringWithShadow("Go => " + (goRight ? "right" : "left"), 2, 2 + (i++ * fr.FONT_HEIGHT), -1);
+        fr.drawStringWithShadow("Dodo => " + (dodo > 0), 2, 2 + (i++ * fr.FONT_HEIGHT), -1);
+
+    }
+
+    @Subscribe
     public void onRender3D(EventRender.R3D eventRender) {
         EnumFacing facing = EnumFacing.values()[SkyBot.INSTANCE.getConfig().enumFacingOrd + 2];
 
-        BlockPos bussy = mc.thePlayer.getPosition().add(facing.getDirectionVec().getZ() + 0.5f, 0, facing.getDirectionVec().getX() + 0.5f);
-        AxisAlignedBB bb = new AxisAlignedBB(bussy.getX() - 0.5f, bussy.getY() - 0.5f, bussy.getZ() - 0.5f, bussy.getX() + 0.5f, bussy.getY() + 0.5f, bussy.getZ() + 0.5f);
+        BlockPos[] positions = getPositions();
 
-        GL11.glPushMatrix();
-        GL11.glColor4d(1, 1, 1, 1);
-        RenderUtils.init3D();
-        RenderUtils.drawOutlinedBox(bb);
+        Renderer.startLines(new Color(0, 0, 255, 255), 1, true);
 
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldRenderer = tessellator.getWorldRenderer();
+        BlockPos pos;
+        AxisAlignedBB aabb;
 
-        RenderUtils.reset3D();
-        GL11.glPopMatrix();
+        pos = positions[0];
+        aabb = new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1);
+        Renderer.glColor(new Color(0, 255, 0, 255), 1);
+        Renderer.drawAABB(aabb);
+
+        pos = positions[1];
+        aabb = new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1);
+        Renderer.glColor(new Color(255, 0, 0, 255), 1);
+        Renderer.drawAABB(aabb);
+
+        Renderer.endLines(true);
+    }
+
+    /**
+     * @return Left / right
+     */
+    private BlockPos[] getPositions() {
+        BlockPos[] positions = new BlockPos[2];
+        EnumFacing[] sides = new EnumFacing[2];
+        EnumFacing facing = EnumFacing.values()[SkyBot.INSTANCE.getConfig().enumFacingOrd + 2];
+        BlockPos playerPos = new BlockPos(
+                mc.thePlayer.posX,
+                mc.thePlayer.posY + 1,
+                mc.thePlayer.posZ
+        );
+
+        switch (facing) {
+            case NORTH:
+                sides[0] = EnumFacing.WEST;
+                sides[1] = EnumFacing.EAST;
+                break;
+            case SOUTH:
+                sides[0] = EnumFacing.EAST;
+                sides[1] = EnumFacing.WEST;
+                break;
+            case WEST:
+                sides[0] = EnumFacing.SOUTH;
+                sides[1] = EnumFacing.NORTH;
+                break;
+            case EAST:
+                sides[0] = EnumFacing.NORTH;
+                sides[1] = EnumFacing.SOUTH;
+                break;
+        }
+        positions[0] = playerPos.offset(sides[0]);
+        positions[1] = playerPos.offset(sides[1]);
+
+        return positions;
     }
 }
---
+++ src/main/java/me/xtrm/skybot/utils/Renderer.java
@@ -0,0 +1,92 @@
+package me.xtrm.skybot.utils;
+
+import lombok.experimental.Helper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.AxisAlignedBB;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Renderer {
+
+    private final static Tessellator tessellator = Tessellator.getInstance();
+    private final static WorldRenderer buffer = tessellator.getWorldRenderer();
+    private final static RenderManager renderManager = Minecraft.getMinecraft().getRenderManager();
+
+    public static void glColor(Color color, float alpha) {
+        float[] colorComponents = color.getColorComponents(null);
+        GlStateManager.color(colorComponents[0], colorComponents[1], colorComponents[2], alpha);
+    }
+
+    public static void startLines(Color color, float alpha, float lineWidth, boolean ignoreDepth) {
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO);
+        glColor(color, alpha);
+        GL11.glLineWidth(lineWidth);
+        GlStateManager.disableTexture2D();
+        GlStateManager.depthMask(false);
+
+        if (ignoreDepth) {
+            GlStateManager.disableDepth();
+        }
+    }
+
+    public static void startLines(Color color, float lineWidth, boolean ignoreDepth) {
+        startLines(color, .4f, lineWidth, ignoreDepth);
+    }
+
+    public static void endLines(boolean ignoredDepth) {
+        if (ignoredDepth) {
+            GlStateManager.enableDepth();
+        }
+
+        GlStateManager.depthMask(true);
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+    }
+
+    public static void drawAABB(AxisAlignedBB aabb) {
+        AxisAlignedBB toDraw = aabb.offset(-renderManager.viewerPosX, -renderManager.viewerPosY, -renderManager.viewerPosZ);
+
+        buffer.begin(GL_LINES, DefaultVertexFormats.POSITION);
+        // bottom
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.minZ).endVertex();
+        // top
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.minZ).endVertex();
+        // corners
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.minZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.maxX, toDraw.maxY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.minY, toDraw.maxZ).endVertex();
+        buffer.pos(toDraw.minX, toDraw.maxY, toDraw.maxZ).endVertex();
+        tessellator.draw();
+    }
+
+    public static void drawAABB(AxisAlignedBB aabb, double expand) {
+        drawAABB(aabb.expand(expand, expand, expand));
+    }
+}
